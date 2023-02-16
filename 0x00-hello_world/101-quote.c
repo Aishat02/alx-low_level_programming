bb12f7f91a4cf931f25bfedcd5f75fb3cbc55845
titilayo <titilayotijani15@gmail.com>
2023-02-16 13:46:31 -0800

    fputs function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,8 +8,6 @@
  */
 int main(void)
 {
-	`
-
-	vprintf("and that piece of art is useful"" - Dora Korpar, 2015-10-19");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
