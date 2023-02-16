98956d2c4d1e27625698e5e4b5b1226d6d509d1d
titilayo <titilayotijani15@gmail.com>
2023-02-16 13:16:07 -0800

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always i (Success)
+ */
+int main(void)
+{
+	`
+
+	vprintf("and that piece of art is useful"" - Dora Korpar, 2015-10-19");
+	return (1);
+}
