74bac6be8496878fa05bb66ae86743a6b54fd3e0
titilayo <titilayotijani15@gmail.com>
2023-02-22 03:25:59 -0800

    betty style

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,20 +7,18 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
-	/* this lines causes the output to go into an infinite loop
-        while (i < 10)
-        {
-                putchar(i);
-        }
-	*/
+	/* while (i < 10) */
+	{
+		putchar(i);
+	}
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
