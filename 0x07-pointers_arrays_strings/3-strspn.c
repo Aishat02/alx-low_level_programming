3963ce93280f560bfa3bbf5fa8eee2a634893b38
titilayo <titilayotijani15@gmail.com>
2023-03-06 08:53:36 -0800

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: memory area pointed to
+ * @n: number of bytes
+ *
+ * Return: numbers of bytes in the initail segement of s,
+ * which consists only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int q = 0;
+	while (*s)
+	{
+		int a = 0;
+		int z;
+
+		for (z = 0; accept[z]; z++)
+		{
+			if (*s == accept[z])
+			{
+				a = 1;
+				break;
+			}
+		}
+		if(a)
+		{
+			q++;
+		}
+		else
+		{
+			break;
+		}
+		s++;
+	}
+	return (q);
+}
